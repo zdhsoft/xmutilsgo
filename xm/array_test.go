429ae42c95062e1ae69908513903b4c4bb478a78
zdhsoft <zdhsoft@163.com>
2024-08-27 23:56:49 +0800

    增加去重测试用例

--- xm/array_test.go
+++ xm/array_test.go
@@ -1,7 +1,7 @@
 package xm
 
 import (
-	"sort"
+	"slices"
 	"testing"
 )
 
@@ -10,8 +10,34 @@ func Test_ArraySort(t *testing.T) {
 	for i := 0; i < 20; i++ {
 		lst = append(lst, RandomIntScope(1, 100000))
 	}
-	t.Log("----", lst)
-	sort.Ints(lst)
-	t.Log(">>>>", lst)
+	ArraySort(lst)
+	if !slices.IsSorted(lst) {
+		t.Errorf("排序后的结果不是有序的！")
+	}
 	//  ArraySort<[]int>(lst)
+}
+
+func Test_Deduplicate(t *testing.T) {
+	hasDup := func(arr []int) (bool, int) {
+		seen := make(map[int]bool)
+		for _, value := range arr {
+			if seen[value] {
+				return true, value
+			}
+			seen[value] = true
+		}
+		return false, 0
+	}
+	lst := make([]int, 0, 20)
+	for i := 0; i < 20; i++ {
+		lst = append(lst, RandomIntScope(1, 5))
+	}
+	newList := Deduplicate(lst)
+	t.Log(lst)
+	t.Log(newList)
+	if has, value := hasDup(newList); has {
+		t.Errorf("存在重复的元素：%d", value)
+		return
+	}
+
 }
